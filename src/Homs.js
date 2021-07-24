08badea4ce8913b5d4f44999e6752b3f8004e981
mdarif6 <md1arif015@gmail.com>
2021-07-24 10:40:30 +0530

    map method

--- src/Homs.js
+++ src/Homs.js
@@ -1,21 +1,34 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [name, setName] = useState("mario");
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName("luigi");
-    setAge(30);
-  };
-
+  const [bolgs, setBlogs] = useState([
+    {
+      title: "My new website",
+      body: " lorem ipsum...",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome party!",
+      body: " lorem ipsum...",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: " lorem ipsum...",
+      author: "jian",
+      id: 3,
+    },
+  ]);
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>
-        {name} is {age} old
-      </p>
-      <button onClick={handleClick}>Click Me</button>
+    <div className="home">
+      {bolgs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
